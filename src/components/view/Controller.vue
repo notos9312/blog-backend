f5e3b12ebf62356b34baa6d6a91dda7422204748
Notos <lianggh159@outlook.com>
2017-12-05 01:14:33 +0800

    almost complete controller page style

--- src/components/view/Controller.vue
+++ src/components/view/Controller.vue
@@ -1,24 +1,47 @@
 <template>
   <div id="controllerVue">
-    <el-row id="controllerWrap">
-      <el-container>
-        <el-aside>
-          <div id="userMsg">
-            <div id="avator"></div>
-          </div>
-          <el-menu :default-openeds="['1', '2']" style="margin-top:1px;">
-            <el-submenu index="1">
-              <template slot="title">发布总览</template>
-              <el-menu-item index="1-1" style="padding-left:50px;">记事</el-menu-item>
-              <el-menu-item index="1-2" style="padding-left:50px;">吐槽</el-menu-item>
-            </el-submenu>
-            <el-submenu index="2">
-              <template slot="title">实验场</template>
-                <el-menu-item index="2-1" style="padding-left:50px;">敬请期待</el-menu-item>
-            </el-submenu>
-          </el-menu>
-        </el-aside>
-      </el-container>
+    <el-row>
+      <el-col :span="3"></el-col>
+      <el-col id="controllerWrap" :span="18">
+        <el-row>
+          <el-col :span="7" style="box-sizing:border-box; padding-top:90px;">
+            <div id="userMsg">
+              <div id="avatar"><img width="100%" height="100%" src="../../assets/images/avatar.png" alt="my avatar"></div>
+            </div>
+            <el-aside style="width:100%; height:100%;">
+              <el-menu :default-openeds="['1', '2']">
+                <el-submenu index="1">
+                  <template slot="title">发布总览</template>
+                  <el-menu-item index="1-1" style="padding-left:55px;">记事</el-menu-item>
+                  <el-menu-item index="1-2" style="padding-left:55px;">吐槽</el-menu-item>
+                </el-submenu>
+                <el-submenu index="2">
+                  <template slot="title">实验场</template>
+                    <el-menu-item index="2-1" style="padding-left:55px;">敬请期待</el-menu-item>
+                </el-submenu>
+                <el-submenu index="3">
+                  <template slot="title">个人资料</template>
+                    <el-menu-item index="3-1" style="padding-left:55px;">基本资料</el-menu-item>
+                </el-submenu>
+              </el-menu>
+            </el-aside>
+          </el-col>
+          <el-col :span="17" style="box-sizing:border-box; padding-top:90px;">
+            <div id="oprateNav" style="color:#fff;">
+              <div id="location" style="float:left; height:100%; margin:30px;">
+                <span style="vertical-align:middle;"><a href="" style="color:#fff;">总览</a> - <a href="" style="color:#fff;">记事</a></span>
+              </div>
+              <div id="operateBtn" style="float:right; height:100%; margin:30px;">
+                <a href="" style="color:#fff;">新增</a> / <a href="" style="color:#fff;">删除</a>
+              </div>
+            </div>
+            <div>
+              <p>{{hello}}</p>
+            </div>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="3"></el-col>
     </el-row>
     <not-footer></not-footer>
   </div>
@@ -26,23 +49,23 @@
 
 <script>
 import NotFooter from '@/components/public/NotFooter'
+
 export default {
+  components: {
+    NotFooter
+  },
   data() {
     return {
       hello: 'hello?'
     }
   },
-  created: function(){
-    console.log('created')
+  created: function() {
     var _this = this
     this.$http.get('/api/hello').then(res => {
       _this.hello = res.body
     }, err => {
       _this.hello = err.status
     })
-  },
-  components: {
-    NotFooter
   }
 }
 </script>
@@ -54,61 +77,64 @@ export default {
     text-align: center;
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   }
-
-  #controllerWrap {
+  #controllerVue>.el-row {
     position: relative;
     top: 5%;
-    width: 90%;
+    width: 100%;
     height: 86%;
     margin: 0 auto;
+  }
+  #controllerWrap{
     background-color: rgba(255, 255,255, 0.9);
     border: 0;
     border-radius: 4px;
   }
-
-  #controllerWrap>.el-container {
-    position: relative;
-    width: 99%;
-    height: 98%;
-    top: 1%;
-    margin: 0 auto;
-    border: 1px solid #eee;
+  #controllerWrap>.el-row{
+    height: 100%;
   }
-
-  #controllerWrap>.el-container>.el-aside {
-    background-color: rgb(233, 233, 233);
-    width: 35%;
+  .el-col {
+    height: 100%;
   }
-
   #userMsg {
+    position: absolute;
+    top: 0;
     height: 90px;
-    /* background-color: #B3C0D1; */
-    border-bottom: 1px solid #EDF2FC;
+    width: 29.2%;
+    background-color: #409EFF;
+    border: 0;
+    border-radius: 4px 0 0 0;
   }
-
-  #avator {
+  #avatar {
     width: 80px;
     height: 80px;
     margin-top: 5px;
-    background-color: blue;
     display:inline-block;
     vertical-align: middle;
     cursor: pointer;
   }
-
-  .el-aside {
-    color: #333;
+  #oprateNav {
+    position: absolute;
+    top: 0;
+    height: 90px;
+    width: 70.8%;
+    background-color: #409EEE;
+    border: 0;
+    border-radius: 0 4px 0 0;
+    /* padding-top: 30px; */
   }
-
+  .el-aside {
+    background-color: white;
+    border: 0;
+    border-radius: 0 0 0 4px;
+  }
+  .el-menu {
+    border-right:0;
+  }
 </style>
 
 <style>
   .el-submenu__title {
     font-size: 18px;
   }
-  .el-menu-item {
-    font-size: 16px;
-  }
 </style>
-
 
