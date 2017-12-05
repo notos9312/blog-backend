9fe9332d182c435b673e94de11cc19678f842e37
Notos <lianggh159@outlook.com>
2017-12-06 00:56:50 +0800

    create table panel vue

---
+++ src/components/public/TablePanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="tablePanelVue">
+    <span>{{'panelType: '+panelType}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['panelType'],
+  watch: {
+    panelType: function(newVal, oldVal){
+      this.$message('old: '+oldVal+', new: '+newVal)
+    }
+  }
+}
+</script>
+
