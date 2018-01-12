7100ddb030e985ff2bd6ddad5b31618a8e10c1fa
lianggh <lianggh@bairuitech.com>
2018-01-12 17:50:16 +0800

    add cors
    Change-Id: I5b4761cac4dc419fb10dc2e1852de6bdee89aa24

--- server.js
+++ server.js
@@ -11,21 +11,22 @@ var app = express()
 app.use(express.static(__dirname + '/src'))
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With")
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+  res.header("X-Powered-By", ' 3.2.1')
+  if (req.method == "OPTIONS") res.send(200) /*让options请求快速返回*/
+  else next()
+})
+
 app.get('/api/hello', function(req, res){
   res.send('Hello you~')
-  // var tmp = {
-  //   createTime: new Date().getTime(),
-  //   contentType: 'articles',
-  //   title: 'my title',
-  //   content: 'my content'
-  // }
-  // insertContent(tmp, function(error, result){
-  //   if (error) {
-  //     res.send(error)
-  //   } else {
-  //     res.send(result)
-  //   }
-  // })
+  // res.send(JSON.stringify(req.ips))
+})
+
+app.post('/api/check', function(req, res){
+  res.send(req)
 })
 
 // app.post('/api/addUser', urlencodedParser, function(req, res){
