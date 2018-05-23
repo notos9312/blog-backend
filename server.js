5fecbafcb605a7642317c4974ed92ae77caafecf
Notos <lianggh159@outlook.com>
2018-05-23 16:01:16 +0800

    取消跨域设置

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ var app = express()
 app.use(express.static(__dirname + '/src'))
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.all('*', function (req, res, next) {
+/* app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With")
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   res.header("X-Powered-By", ' 3.2.1')
-  if (req.method == "OPTIONS") res.send(200) /*让options请求快速返回*/
+  if (req.method == "OPTIONS") res.send(200) //让options请求快速返回
   else next()
-})
+}) */
 
 app.get('/api/hello', function(req, res){
   res.send('Hello you~')
