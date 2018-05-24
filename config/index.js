5c6f6a33ed9a25e756adcc82fd20e8a26bdac085
Notos <lianggh159@outlook.com>
2018-05-24 12:29:04 +0800

    change proxyTable for test server

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target: 'http://localhost:2333/api/',
+        target: 'http://localhost:2334/api/',
         changeOrigin: true,
         pathRewrite: {
           '^/api':'/'
